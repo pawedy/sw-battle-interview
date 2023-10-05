fb844b1b02aa96d208c74cc0a2474cea2efaaf37
Paweł Dymarkowski <pawel.dymarkowski@gmail.com>
2023-10-05 08:59:21 +0200

    chore: add ngxs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgxsModule, Store } from '@ngxs/store';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MatToolbarModule],
+      imports: [RouterTestingModule, NgxsModule.forRoot([]), MatToolbarModule],
       declarations: [AppComponent],
     })
   );
@@ -15,6 +16,11 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('store should be available', () => {
+    const store = TestBed.inject(Store);
+    expect(store).toBeTruthy();
   });
 
   it(`should have as title 'sw-battle-interview'`, () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
